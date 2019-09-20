05f4f37326d60cd716be97fff6322e32c1cb448d
FuzzyFade <1339184537@qq.com>
2019-09-20 18:40:47 +0800

    :rocket: 部署真正的拼图

--- src/lib/pictures.js
+++ src/lib/pictures.js
@@ -1,5 +1,5 @@
 export const pictures = [
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562336127688&di=8b3b64da7ea88ddb1a14b95b9ba2e7cc&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F29%2F20170529020618_QMZXK.jpeg",
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568833453259&di=e640bd9f968a401517cf30b872f056a5&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201708%2F10%2F20170810174510_5rPtn.jpeg",
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562930803&di=efa158df98e214314b3bc5c235fbba81&imgtype=jpg&er=1&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201804%2F23%2F20180423194810_yywff.jpg"
+  "https://icecee.oss-cn-shanghai.aliyuncs.com/piliili/d3.png",
+  "https://icecee.oss-cn-shanghai.aliyuncs.com/piliili/d4.png",
+  "https://icecee.oss-cn-shanghai.aliyuncs.com/piliili/d5.png"
 ];
