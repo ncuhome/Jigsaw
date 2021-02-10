e3ebf2c0e59bc089218a213ceeb6ae008dd79845
FuzzyFade <1339184537@qq.com>
2021-02-11 01:24:45 +0800

    feat: 替换 lodash-es

--- src/lib/helloText.js
+++ src/lib/helloText.js
@@ -1,8 +1,6 @@
-import _ from 'lodash'
+import { random } from 'lodash-es';
 
-export default function halo(){
-  const haloText = [
-    'Aloha!','Hallo!','Hello!','你来啦，','你好啊，'
-  ];
-  return haloText[_.random(0, haloText.length - 1)]
-};
+export default function halo() {
+  const haloText = ['Aloha!', 'Hallo!', 'Hello!', '你来啦，', '你好啊，'];
+  return haloText[random(0, haloText.length - 1)];
+}
